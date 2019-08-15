e02fdf386f8a4822a47405396fe16e92f9660ad8
Gem Saunders <gemmasaunders@mmc.school.nz>
2019-08-15 14:29:45 +1200

    Added a name option

--- catch the chungus/FrmAlien.Designer.cs
+++ catch the chungus/FrmAlien.Designer.cs
@@ -41,6 +41,8 @@
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.MnuStart = new System.Windows.Forms.ToolStripMenuItem();
             this.MnuQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
             this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -73,7 +75,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(373, 182);
+            this.label2.Location = new System.Drawing.Point(373, 331);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 24);
             this.label2.TabIndex = 2;
@@ -83,7 +85,8 @@
             // 
             this.LblScore.BackColor = System.Drawing.Color.White;
             this.LblScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LblScore.Location = new System.Drawing.Point(377, 218);
+            this.LblScore.Font = new System.Drawing.Font("Prestige Elite Std", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblScore.Location = new System.Drawing.Point(377, 355);
             this.LblScore.Name = "LblScore";
             this.LblScore.Size = new System.Drawing.Size(60, 30);
             this.LblScore.TabIndex = 3;
@@ -106,17 +109,18 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(359, 101);
+            this.label3.Location = new System.Drawing.Point(364, 227);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.Size = new System.Drawing.Size(92, 24);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Time Left";
+            this.label3.Text = " Time Left";
             // 
             // LblTime
             // 
             this.LblTime.BackColor = System.Drawing.Color.White;
             this.LblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LblTime.Location = new System.Drawing.Point(373, 125);
+            this.LblTime.Font = new System.Drawing.Font("Prestige Elite Std", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTime.Location = new System.Drawing.Point(377, 262);
             this.LblTime.Name = "LblTime";
             this.LblTime.Size = new System.Drawing.Size(60, 30);
             this.LblTime.TabIndex = 7;
@@ -147,11 +151,30 @@
             this.MnuQuit.Text = "QUIT";
             this.MnuQuit.Click += new System.EventHandler(this.MnuQuit_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Name";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(356, 160);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(100, 20);
+            this.TxtName.TabIndex = 10;
+            this.TxtName.TextChanged += new System.EventHandler(this.TxtName_TextChanged);
+            // 
             // FrmAlien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.LblTime);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.LblScore);
@@ -186,6 +209,8 @@
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.ToolStripMenuItem MnuStart;
         private System.Windows.Forms.ToolStripMenuItem MnuQuit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtName;
     }
 }
 
